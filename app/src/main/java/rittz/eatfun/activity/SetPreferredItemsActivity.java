fa4fa5690d272eeb4fb20032816b6e484615e085
Ian Graves <ianbgraves@gmail.com>
2015-10-17 13:04:34 -0700

    Added autocomplete when adding new orders

--- app/src/main/java/rittz/eatfun/activity/SetPreferredItemsActivity.java
+++ app/src/main/java/rittz/eatfun/activity/SetPreferredItemsActivity.java
@@ -2,22 +2,30 @@ package rittz.eatfun.activity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import retrofit.RetrofitError;
+import rittz.eatclub.api.model.Menu;
 import rittz.eatfun.R;
+import rittz.eatfun.core.EatclubInstance;
 import rittz.eatfun.storage.Config;
 
 public class SetPreferredItemsActivity extends AppCompatActivity {
@@ -25,6 +33,7 @@ public class SetPreferredItemsActivity extends AppCompatActivity {
     @Bind(R.id.item_container) LinearLayout linearLayout;
 
     private List<String> items = new ArrayList<>();
+    private List<String> foodItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +44,9 @@ public class SetPreferredItemsActivity extends AppCompatActivity {
         for (String item : Config.get().getPreferredItems()) {
             addItem(item);
         }
+
+        // Auto completeness
+        new FoodItemFetch().execute();
     }
 
     @Override
@@ -67,9 +79,14 @@ public class SetPreferredItemsActivity extends AppCompatActivity {
         builder.setTitle("Specify Item");
 
         // Set up the input
-        final EditText input = new EditText(this);
+        final AutoCompleteTextView input = new AutoCompleteTextView(this);
         input.setInputType(InputType.TYPE_CLASS_TEXT);
         builder.setView(input);
+
+        if (foodItems.size() > 0) {
+            final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, foodItems);
+            input.setAdapter(adapter);
+        }
 
         // Set up the buttons
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -87,5 +104,35 @@ public class SetPreferredItemsActivity extends AppCompatActivity {
         });
 
         builder.show();
+    }
+
+    // Lazy man AsyncTask way - who cares about memory leaks!
+
+    public class FoodItemFetch extends AsyncTask<Void, Void, Set<String>> {
+
+        @Override
+        protected Set<String> doInBackground(Void... params) {
+            try {
+                final List<Menu> menus = EatclubInstance.get().menus();
+
+                final Set<String> foodNames = new HashSet<>();
+                for (Menu menu : menus) {
+                    for (Menu.Item item : menu.getItems()) {
+                        foodNames.add(item.getItem());
+                    }
+                }
+
+                return foodNames;
+            }
+            catch (RetrofitError ex) {
+                return Collections.EMPTY_SET;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Set<String> strings) {
+            super.onPostExecute(strings);
+            foodItems.addAll(strings);
+        }
     }
 }
